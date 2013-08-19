0bc9d69ddd30bae12990d0c41e2c4d3f1bffed6c
Alex Muntada <alexm@alexm.org>
2013-08-19 12:01:36 +0200

    refactor Rental.frequent_renter_points out of Customer.statement

---
+++ ruby/page_019/customer.rb
@@ -0,0 +1,27 @@
+require 'movie'
+
+class Customer
+	attr_reader :name
+	def initialize(name)
+		@name = name
+		@rentals = []
+	end
+	def add_rental(arg)
+		@rentals << arg
+	end
+	def statement
+		total_amount, frequent_renter_points = 0, 0
+		result = "Rental Record for #{@name}\n"
+		@rentals.each do |element|
+			frequent_renter_points += element.frequent_renter_points
+
+			# show figures for this rental
+			result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
+			total_amount += element.charge
+		end
+		# add footer lines
+		result += "Amount owed is #{total_amount}\n"
+		result += "You earned #{frequent_renter_points} frequent renter points"
+		result
+	end
+end
