540e44be2a9f2ed5e63b3d6a01a5c5f34160264f
Alex Muntada <alexm@alexm.org>
2013-08-19 11:52:00 +0200

    refactor Rental.charge out of Customer.statement

---
+++ ruby/page_017/customer.rb
@@ -0,0 +1,33 @@
+require 'movie'
+
+class Customer
+	attr_reader :name
+	def initialize(name)
+		@name = name
+		@rentals = []
+	end
+	def add_rental(arg)
+		@rentals << arg
+	end
+	def statement
+		total_amount, frequent_renter_points = 0, 0
+		result = "Rental Record for #{@name}\n"
+		@rentals.each do |element|
+
+			# add frequent renter points
+			frequent_renter_points += 1
+			# add bonus for a two day new release rental
+			if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
+				frequent_renter_points += 1
+			end
+
+			# show figures for this rental
+			result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
+			total_amount += element.charge
+		end
+		# add footer lines
+		result += "Amount owed is #{total_amount}\n"
+		result += "You earned #{frequent_renter_points} frequent renter points"
+		result
+	end
+end
