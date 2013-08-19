13ca706e727eec7afeb135484252225955b09058
Alex Muntada <alexm@alexm.org>
2013-08-19 21:02:28 +0200

    refactor Customer.total_charge

---
+++ ruby/page_023/customer.rb
@@ -0,0 +1,36 @@
+require 'movie'
+
+class Customer
+	attr_reader :name
+	def initialize(name)
+		@name = name
+		@rentals = []
+	end
+	def add_rental(arg)
+		@rentals << arg
+	end
+	def statement
+		frequent_renter_points = 0
+		result = "Rental Record for #{@name}\n"
+		@rentals.each do |element|
+			frequent_renter_points += element.frequent_renter_points
+
+			# show figures for this rental
+			result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
+		end
+		# add footer lines
+		result += "Amount owed is #{total_charge}\n"
+		result += "You earned #{frequent_renter_points} frequent renter points"
+		result
+	end
+
+    private
+
+    def total_charge
+        result = 0
+        @rentals.each do |element|
+            result += element.charge
+        end
+        result
+    end
+end
---
+++ ruby/page_025/customer.rb
@@ -0,0 +1,32 @@
+require 'movie'
+
+class Customer
+	attr_reader :name
+	def initialize(name)
+		@name = name
+		@rentals = []
+	end
+	def add_rental(arg)
+		@rentals << arg
+	end
+	def statement
+		frequent_renter_points = 0
+		result = "Rental Record for #{@name}\n"
+		@rentals.each do |element|
+			frequent_renter_points += element.frequent_renter_points
+
+			# show figures for this rental
+			result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
+		end
+		# add footer lines
+		result += "Amount owed is #{total_charge}\n"
+		result += "You earned #{frequent_renter_points} frequent renter points"
+		result
+	end
+
+    private
+
+    def total_charge
+        @rentals.inject(0) { |sum,rental| sum + rental.charge }
+    end
+end
