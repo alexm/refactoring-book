af1da8cd742feda5bf47be9a23cb89bdeab23d54
Alex Muntada <alexm@alexm.org>
2014-02-21 21:44:06 +0100

    fix indentation issues

--- ruby/test/tc_statement.rb
+++ ruby/test/tc_statement.rb
@@ -12,22 +12,22 @@ class StatementTest < Test::Unit::TestCase
         @customer = Customer.new('alexm')
         @customer.add_rental(rental)
     end
-	def test_statement
+    def test_statement
         statement_exp = <<EoS.chomp
 Rental Record for alexm
 \tThe Watchmen\t126
 Amount owed is 126
 You earned 2 frequent renter points
 EoS
-		assert_equal statement_exp, @customer.statement
+        assert_equal statement_exp, @customer.statement
     end
-	def test_html_statement
-	    html_statement_exp = <<EoS.chomp
+    def test_html_statement
+        html_statement_exp = <<EoS.chomp
 <h1>Rentals for <em>alexm</em></h1><p>
 \tThe Watchmen: 126<br>
 <p>You owe <em>126</em><p>
 On this rental you earned <em>2</em> frequent renter points<p>
 EoS
-		assert_equal html_statement_exp, @customer.html_statement if @customer.respond_to?(:html_statement)
+        assert_equal html_statement_exp, @customer.html_statement if @customer.respond_to?(:html_statement)
     end
 end
