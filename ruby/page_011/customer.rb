1080ae412e82d4d4b76f9f1b4dce73a69193b133
Alex Muntada <alexm@alexm.org>
2014-02-23 01:06:26 +0100

    refactor this_amount into result in Customer.charge

--- ruby/page_011/customer.rb
+++ ruby/page_011/customer.rb
@@ -33,16 +33,16 @@ class Customer
 	end
 
     def amount_for(rental)
-        this_amount = 0
+        result = 0
         case rental.movie.price_code
         when Movie::REGULAR
-            this_amount += 2
-            this_amount += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
+            result += 2
+            result += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
         when Movie::NEW_RELEASE
-            this_amount += rental.days_rented * 3
+            result += rental.days_rented * 3
         when Movie::CHILDRENS
-            this_amount += 1.5
-            this_amount += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
+            result += 1.5
+            result += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
         end
     end
 end
