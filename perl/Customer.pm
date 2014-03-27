0cd836bf302326004b8529d0345924e767fa6e4d
Alex Muntada <alexm@alexm.org>
2014-03-27 14:49:30 +0100

    page 11

--- perl/Customer.pm
+++ perl/Customer.pm
@@ -49,23 +49,23 @@ sub statement
 
 sub amount_for
 {
-    my ($element) = @_;
+    my ($rental) = @_;
 
-    my $this_amount = 0;
+    my $result = 0;
     # determine amounts for each line
-    if ( $element->movie->price_code == Movie::REGULAR ) {
-        $this_amount += 2;
-        $this_amount += ( $element->days_rented - 2 ) * 1.5 if $element->days_rented > 2;
+    if ( $rental->movie->price_code == Movie::REGULAR ) {
+        $result += 2;
+        $result += ( $rental->days_rented - 2 ) * 1.5 if $rental->days_rented > 2;
     }
-    elsif ( $element->movie->price_code == Movie::NEW_RELEASE ) {
-        $this_amount += $element->days_rented * 3;
+    elsif ( $rental->movie->price_code == Movie::NEW_RELEASE ) {
+        $result += $rental->days_rented * 3;
     }
-    elsif ( $element->movie->price_code == Movie::CHILDRENS ) {
-        $this_amount += 1.5;
-        $this_amount += ( $element->days_rented - 3 ) * 1.5 if $element->days_rented > 3;
+    elsif ( $rental->movie->price_code == Movie::CHILDRENS ) {
+        $result += 1.5;
+        $result += ( $rental->days_rented - 3 ) * 1.5 if $rental->days_rented > 3;
     }
 
-    return $this_amount;
+    return $result;
 }
 
 1;
