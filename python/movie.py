50a0b0dfa6fc11c6ee19827c5d5a64a7f8412238
Alex Muntada <alex.muntada@gmail.com>
2025-01-27 18:08:04 +0100

    Replace conditional with polymorphism on Movie.charge

--- python/movie.py
+++ python/movie.py
@@ -18,13 +18,19 @@ class Movie:
     @price_code.setter
     def price_code(self, value):
         self._price_code = value
+        match value:
+            case Movie.REGULAR:
+                self._price = RegularPrice()
+            case Movie.NEW_RELEASE:
+                self._price = NewReleasePrice()
+            case Movie.CHILDRENS:
+                self._price = ChildrensPrice()
 
     def charge(self, days_rented):
         result = 0
         match self.price_code:
             case Movie.REGULAR:
-                result += 2
-                if days_rented > 2: result += (days_rented - 2) * 1.5
+                result = self._price.charge(days_rented)
             case Movie.NEW_RELEASE:
                 result += days_rented * 3
             case Movie.CHILDRENS:
@@ -34,3 +40,18 @@ class Movie:
 
     def frequent_renter_points(self, days_rented):
         return 2 if self.price_code == Movie.NEW_RELEASE and days_rented > 1 else 1
+
+
+class RegularPrice:
+    @staticmethod
+    def charge(days_rented):
+        result = 2
+        if days_rented > 2:
+            result += (days_rented - 2) * 1.5
+        return result
+
+class NewReleasePrice:
+    pass
+
+class ChildrensPrice:
+    pass
